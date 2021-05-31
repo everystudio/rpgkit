f2503aefdaf9ac5f42c284303da26fcb4ef386b9
watanabe@tiffa <watanabe.everystudio@gmail.com>
2021-05-31 22:04:57 +0900

    バトル関連の処理を追加

--- Assets/Scripts/BattleSystem/BattleHUD.cs
+++ Assets/Scripts/BattleSystem/BattleHUD.cs
@@ -9,4 +9,8 @@ public class BattleHUD : MonoBehaviour
 	public Button m_btnSkill;
 	public Button m_btnItem;
 	public Button m_btnBack;
+
+	public GameObject m_goRootPartyCard;
+	public GameObject m_prefPartyCard;
+
 }
--- Assets/Scripts/BattleSystem/BattleMain.cs
+++ Assets/Scripts/BattleSystem/BattleMain.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using rpgkit;
 public class BattleMain : StateMachineBase<BattleMain>
 {
 	[SerializeField] private GameObject m_goRootBattleLog;
@@ -24,14 +24,22 @@ public class BattleMain : StateMachineBase<BattleMain>
 		m_prefBattleLog.SetActive(false);
 		foreach( Transform tf in m_goRootBattleLog.transform.GetComponentsInChildren<Transform>())
 		{
-			Debug.Log(tf.name);
+			//Debug.Log(tf.name);
+			Destroy(tf.gameObject);
 		}
 	}
-
 
 	private void Start()
 	{
 		SetState(new BattleMain.Standby(this));
+	}
+	private void OnEnable()
+	{
+		m_inputDebugBattleStart.Enable();
+	}
+	private void OnDisable()
+	{
+		m_inputDebugBattleStart.Disable();
 	}
 
 	private class Standby : StateBase<BattleMain>
@@ -44,14 +52,12 @@ public class BattleMain : StateMachineBase<BattleMain>
 		{
 			base.OnEnterState();
 			machine.m_inputDebugBattleStart.performed += M_inputDebugBattleStart_performed;
-			machine.m_inputDebugBattleStart.Enable();
 		}
 
 		public override void OnExitState()
 		{
 			base.OnExitState();
 			machine.m_inputDebugBattleStart.performed -= M_inputDebugBattleStart_performed;
-			machine.m_inputDebugBattleStart.Disable();
 		}
 		private void M_inputDebugBattleStart_performed(InputAction.CallbackContext obj)
 		{
@@ -64,14 +70,17 @@ public class BattleMain : StateMachineBase<BattleMain>
 		public Opening(BattleMain _machine) : base(_machine)
 		{
 			Debug.Log("Opening");
-
 			machine.ClearBattleLog();
-
 			UIAssistant.Instance.ShowPage("Battle");
+		}
+		public override void OnEnterState()
+		{
+			base.OnEnterState();
+
 		}
 		public override void OnUpdateState()
 		{
-			if (machine.m_inputDebugBattleStart.phase == InputActionPhase.Performed)
+			if (machine.m_inputDebugBattleStart.phase == InputActionPhase.Started)
 			{
 				machine.SetState(new BattleMain.TurnStart(machine));
 			}
@@ -101,7 +110,7 @@ public class BattleMain : StateMachineBase<BattleMain>
 		{
 			base.OnEnterState();
 			machine.m_iPlayerCommandIndex = 0;
-			machine.SetState(new BattleMain.CommandTop(machine));
+			machine.SetState(new BattleMain.CommandCheck(machine));
 		}
 	}
 
@@ -201,7 +210,25 @@ public class BattleMain : StateMachineBase<BattleMain>
 		{
 		}
 	}
+	private class CommandCheck : StateBase<BattleMain>
+	{
+		public CommandCheck(BattleMain _machine) : base(_machine)
+		{
+		}
+		public override void OnEnterState()
+		{
+			base.OnEnterState();
 
+			if( DataManager.Instance.m_dataUnit.list.Count <= machine.m_iPlayerCommandIndex)
+			{
+				machine.SetState(new BattleMain.CommandEnd(machine));
+			}
+			else
+			{
+				machine.SetState(new BattleMain.CommandTop(machine));
+			}
+		}
+	}
 	private class CommandTop : StateBase<BattleMain>
 	{
 		public CommandTop(BattleMain _machine) : base(_machine)
@@ -209,6 +236,7 @@ public class BattleMain : StateMachineBase<BattleMain>
 		}
 		public override void OnEnterState()
 		{
+			Debug.Log($"CommandTop:{machine.m_iPlayerCommandIndex}");
 			base.OnEnterState();
 			machine.m_battleHUD.m_btnAttack.onClick.AddListener(() =>
 			{
@@ -218,7 +246,7 @@ public class BattleMain : StateMachineBase<BattleMain>
 					player_index = machine.m_iPlayerCommandIndex,
 					command = "attack"
 				});
-				machine.SetState(new BattleMain.CommandTop(machine));
+				machine.SetState(new BattleMain.CommandCheck(machine));
 			});
 			machine.m_battleHUD.m_btnSkill.onClick.AddListener(() =>
 			{
@@ -264,6 +292,14 @@ public class BattleMain : StateMachineBase<BattleMain>
 	{
 		public ItemSelectTop(BattleMain _machine) : base(_machine)
 		{
+		}
+	}
+
+	private class CommandEnd : StateBase<BattleMain>
+	{
+		public CommandEnd(BattleMain _machine) : base(_machine)
+		{
+			Debug.Log("CommandEnd");
 		}
 	}
 }
